96ed095456b8c6d5536df41f326c70a252d9618b
elbartomojito-maker <elbarto.mojito@gmail.com>
2026-04-22 21:14:43 +0200

    Add complete rb_last_played.py plugin code

--- rb_last_played.py
+++ rb_last_played.py
@@ -1,23 +1,46 @@
-# Last Played Resume Plugin
+"""
+Rhythmbox Last Played Plugin
+===========================
 
-class LastPlayed:
-    def __init__(self):
-        self.last_played = None
+This plugin saves the last played track in Rhythmbox and can restore it on startup.
+"""
 
-    def play(self, item):
-        self.last_played = item
-        print(f'Playing: {item}')
+import gi
+gi.require_version('RhythmDB', '3.0')
+import os
+from gi.repository import RhythmDB
+from gi.repository import GObject
+from gi.repository import Gtk
+from gi.repository import Gdk
 
-    def resume(self):
-        if self.last_played:
-            print(f'Resuming: {self.last_played}')
-        else:
-            print('No item to resume.')
+class LastPlayedPlugin:
+    def __init__(self, rb):
+        self.rb = rb
+        self.db = rb.get_database()
+        self.last_played_file = os.path.expanduser('~/.local/share/rhythmbox/last_played')
 
-# Example usage
-if __name__ == '__main__':
-    lp = LastPlayed()
-    lp.play('Song A')
-    lp.resume()
-    lp.play('Song B')
-    lp.resume()
+        # Load last played track on startup
+        self.load_last_played()
+
+        # Connect to the "playback-started" signal
+        rb.connect('playback-started', self.on_playback_started)
+
+    def on_playback_started(self, player, track):
+        # Save the current track to the last played file
+        self.save_last_played(track)
+
+    def save_last_played(self, track):
+        uri = track.get_string(RhythmDB.PROP_MTP_URI)
+        with open(self.last_played_file, 'w') as f:
+            f.write(uri)
+
+    def load_last_played(self):
+        if os.path.exists(self.last_played_file):
+            with open(self.last_played_file, 'r') as f:
+                uri = f.read().strip()
+                track = self.db.lookup_by_uri(uri)
+                if track:
+                    self.rb.get_player().set_playing_track(track)  
+
+def plugin_init(rb):
+    return LastPlayedPlugin(rb)
